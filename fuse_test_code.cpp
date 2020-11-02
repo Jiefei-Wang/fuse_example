8c298ed7aabc1fbfd4e9e4a9ec240b08d994713d
jiefei <szwjf08@gmail.com>
2020-11-02 00:17:46 -0600

    update

--- fuse_test_code.cpp
+++ fuse_test_code.cpp
@@ -11,19 +11,6 @@
 #include <fuse_lowlevel.h>
 using namespace std;
 
-//function declaration
-void filesystem_thread_func();
-void write_test();
-
-// The virtual file name and size
-#define FILE_NAME "inode2"
-#define FILE_SIZE 1024 * 1024 * 1024
-#define FILE_INODE 2
-
-//Mount point and test file path
-std::string mount_point(std::string(getenv("HOME")) + "/Documents/mount_test");
-std::string test_file_path(mount_point+"/"+FILE_NAME);
-
 //fuse filesystem thread
 std::unique_ptr<std::thread> filesystem_thread(nullptr);
 
@@ -32,13 +19,28 @@ static fuse_chan *channel = NULL;
 static fuse_session *session = NULL;
 static fuse_lowlevel_ops filesystem_operations;
 
+//Mount point and test file path
+std::string mount_point(std::string(getenv("HOME")) + "/Documents/mount_test");
+std::string test_file_path(mount_point+"/"+FILE_NAME);
+
+//The virtual file name and size
+#define FILE_NAME "inode2"
+#define FILE_SIZE 1024 * 1024 * 1024
+#define FILE_INODE 2
+
+//function declaration
+void filesystem_thread_func();
+void write_test();
 
 int main()
 {
     printf("Testing the file in: %s\n", test_file_path.c_str());
+    //Run fuse filesystem in another thread
     filesystem_thread.reset(new std::thread(filesystem_thread_func));
     printf("Press any key to perform the test");
     getchar();
+    //Test the fuse write function
+    //It will freeze the program
     write_test();
     printf("Test finished\n");
     filesystem_thread->join();
@@ -47,12 +49,15 @@ int main()
 
 void write_test()
 {
+    //Open a file handle to a virtual file in the filesystem
     int fd = open(test_file_path.c_str(), O_RDWR);
     if (fd == -1)
     {
         printf("Faile to open the file\n");
         return;
     }
+    //Map the file to the current process
+    //Treat the file data as integer
     int *ptr = (int *)mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (ptr == (void *)-1)
     {
@@ -60,6 +65,7 @@ void write_test()
         close(fd);
         return;
     }
+    //Write data to the file
     uint64_t n = FILE_SIZE / 4;
     uint64_t offset = 1024;
     for (uint64_t i = 0; i < offset; i++)
@@ -70,13 +76,16 @@ void write_test()
             ptr[j] = -j;
         }
     }
+    //Unmap the file
     munmap(ptr, FILE_SIZE);
 }
 
-
-
-
-
+/*
+========================================================
+fuse operations, most are from
+https://github.com/libfuse/libfuse/blob/fuse-2_9_bugfix/example/hello_ll.c
+========================================================
+*/
 static int fill_file_stat(fuse_ino_t ino, struct stat *stbuf)
 {
     stbuf->st_ino = ino;
@@ -209,6 +218,7 @@ static void filesystem_read(fuse_req_t req, fuse_ino_t ino, uint64_t size,
     return;
 }
 
+//The problematic function. It does nothing but allocates the memory
 std::vector<std::unique_ptr<char[]>> ptr_list;
 static void filesystem_write(fuse_req_t req, fuse_ino_t ino, const char *buffer,
                              uint64_t buffer_length, off_t offset, struct fuse_file_info *fi)
@@ -217,7 +227,7 @@ static void filesystem_write(fuse_req_t req, fuse_ino_t ino, const char *buffer,
            (unsigned long long int)offset,
            (unsigned long long int)buffer_length);
     fuse_reply_write(req, buffer_length);
-    //If you comment this out, everything is good
+    //If you comment below out, everything is good
     ptr_list.emplace_back(new char[4096]);
     printf("Finish\n");
 }
@@ -229,6 +239,7 @@ fuse main function
 */
 void filesystem_thread_func()
 {
+    //Create the directory if it does not exist
     mkdir(mount_point.c_str(), 0755);
     filesystem_operations.lookup = filesystem_loopup;
     filesystem_operations.getattr = filesystem_getattr;
